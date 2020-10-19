9865d1399cbbfe224c441ce8027416db06f1838a
ShahnozaSh <shahnoza_3010@mai.ru>
2020-10-19 17:13:43 +0600

    Initial commit

---
+++ src/com/company/Cat.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Cat extends Animal implements Printable{
+    private String breed ;
+
+    public Cat(String breed,  int age, String color) {
+        super(age, color);
+        this.breed = breed;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("breed =" + breed+
+                "\ncolor =" + getColor()+
+                "\nage =" + getAge());
+
+    }
+}
